2a7824f317b2084fb880e521f40956c52b56dafb
foo2810 <anime_117@yahoo.co.jp>
2018-07-19 12:49:02 +0900

    modify slightly

--- sampleOfEmulationOfX86.py
+++ sampleOfEmulationOfX86.py
@@ -10,6 +10,7 @@ from loadPE import *
 from importTable import *
 from exportTable import *
 
+
 class SimpleEngine:
 	def __init__(self, addr):
 		self.capmd = Cs(CS_ARCH_X86, CS_MODE_32)
@@ -22,10 +23,42 @@ class SimpleEngine:
 
 disasm = None
 
+def printReg(mu):
+	r_eax = mu.reg_read(UC_X86_REG_EAX)
+	r_ebx = mu.reg_read(UC_X86_REG_EBX)
+	r_ecx = mu.reg_read(UC_X86_REG_ECX)
+	r_edx = mu.reg_read(UC_X86_REG_EDX)
+	r_ebp = mu.reg_read(UC_X86_REG_EBP)
+	r_esp = mu.reg_read(UC_X86_REG_ESP)
+	r_edi = mu.reg_read(UC_X86_REG_EDI)
+	r_esi = mu.reg_read(UC_X86_REG_ESI)
+	r_eip = mu.reg_read(UC_X86_REG_EIP)
+	r_eflags = mu.reg_read(UC_X86_REG_EFLAGS)
+	r_CS = mu.reg_read(UC_X86_REG_CS)
+	r_SS = mu.reg_read(UC_X86_REG_SS)
+	r_DS = mu.reg_read(UC_X86_REG_DS)
+	r_ES = mu.reg_read(UC_X86_REG_ES)
+	r_FS = mu.reg_read(UC_X86_REG_FS)
+	r_GS = mu.reg_read(UC_X86_REG_GS)
+	print(">>> EAX = 0x%x" % r_eax)
+	print(">>> EBX = 0x%x" % r_ebx)
+	print(">>> ECX = 0x%x" % r_ecx)
+	print(">>> EDX = 0x%x" % r_edx)
+	print(">>> EBP = 0x%x" % r_ebp)
+	print(">>> ESP = 0x%x" % r_esp)
+	print(">>> EDI = 0x%x" % r_edi)
+	print(">>> ESI = 0x%x" % r_esi)
+	print(">>> EIP = 0x%x" % r_eip)
+	print(">>> CS = 0x%x" % r_CS)
+	print(">>> SS = 0x%x" % r_SS)
+	print(">>> DS = 0x%x" % r_DS)
+	print(">>> ES = 0x%x" % r_ES)
+	print(">>> FS = 0x%x" % r_FS)
+	print(">>> GS = 0x%x" % r_GS)
+	
 def hook_block(uc, address, size, user_data):
 	print(">>> Tracing basic block at 0x%x, block size = 0x%x" % (address, size))
 	
-
 def hook_code(uc, address, size, user_data):
 	print(">>> Tracing instruction at 0x%x, instruction size = %u" % (address, size))
 	instruction = uc.mem_read(address, size)
@@ -74,11 +107,11 @@ def main():
 	SIZEOFCODE = peReader.getOptionalHeader().SizeOfCode
 	STACKADDR = 0x600000
 	print("Base Address: " + hex(ADDRESS))
-	print("Entry Point: " + hex(ENTRYPOINT))
-	print("Size of Code: " + hex(SIZEOFCODE))
+	#print("Entry Point: " + hex(ENTRYPOINT))
+	#print("Size of Code: " + hex(SIZEOFCODE))
 	print("Data address: " + hex(STACKADDR))
-	print("Kernel32 base addr: " + hex(addr))
-	print("api base addr: " + hex(a))
+	print("Kernel32 base address: " + hex(addr))
+	print("api base address: " + hex(a))
 	
 	disasm = SimpleEngine(ADDRESS)
 	
@@ -92,26 +125,35 @@ def main():
 		mu.mem_map(ADDRESS, 2 * 1024 * 1024 * 1024)
 		#mu.mem_map(ADDRESS, 2 * 1024 * 1024)
 		#mu.mem_map(STACKADDR, 256 * 1024 * 1024)
-		#mu.mem_map(addr, len(kernel32Data))
+		#mu.mem_map(addr, len(kernel32Data))		
+		
+		#mu.hook_add(UC_HOOK_BLOCK, hook_block)
+		mu.hook_add(UC_HOOK_CODE, hook_code)
 		
 		mu.mem_write(ADDRESS, b"\x00" * 2 * 1024 * 1024 * 1024)
 		mu.mem_write(ADDRESS, dumpData)
 		mu.mem_write(addr, kernel32Data)
 		mu.mem_write(a, api_Data)
 		
-		#mu.hook_add(UC_HOOK_BLOCK, hook_block)
-		mu.hook_add(UC_HOOK_CODE, hook_code)
-		
-		# Initialize machine registers
-		#mu.reg_write(UC_X86_REG_ECX, 0x1234)
-		#mu.reg_write(UC_X86_REG_EDX, 0x7890)
+		# Initialize machine registers		
+		"""
+		print("SET SEGMENT REGISTER")
+		mu.reg_write(UC_X86_REG_ES, 0x002b)
+		print("ES")
+		mu.reg_write(UC_X86_REG_CS, 0x0023)
+		print("CS")
+		mu.reg_write(UC_X86_REG_SS, 0x002b)	#0x0028
+		print("SS")
+		mu.reg_write(UC_X86_REG_DS, 0x002b)
+		print("DS")
+		mu.reg_write(UC_X86_REG_FS, 0x0053)
+		print("FS")
+		mu.reg_write(UC_X86_REG_GS, 0x002b)
+		print("GS")
+		"""
 		mu.reg_write(UC_X86_REG_EBP, STACKADDR + 256 * 1024 * 1024)
 		mu.reg_write(UC_X86_REG_ESP, STACKADDR + 256 * 1024 * 1024)
-		#mu.reg_write(UC_X86_REG_SS, 0x0028)
-		#mu.reg_write(UC_X86_REG_DS, 0x002b)
-		#mu.reg_write(UC_X86_REG_ES, 0x002b)
-		#mu.reg_write(UC_X86_REG_GS, 0x002b)
-		#mu.reg_write(UC_X86_REG_CS, ADDRESS)
+		
 		
 		peImportTable = peReader.getImportTable(dumpData)
 		for ent in peImportTable:
@@ -143,44 +185,15 @@ def main():
 						#print(name + ", ", hex(funcAddr))
 						mu.mem_write(addr + thunk.getHeadPtr(), (a + funcAddr).to_bytes(4, "little"))
 		
-		# Emulate sampCode
+		# Emulation
 		print("[Emulation start]")
 		mu.emu_start(ENTRYPOINT, ENTRYPOINT + SIZEOFCODE)
 		
-		
-		
-		#print("ECX: ", hex(rECX))
-		#print("EDX: ", hex(rEDX))
 	
 	except UcError as e:
 		print(e)
-		r_eax = mu.reg_read(UC_X86_REG_EAX)
-		r_ebx = mu.reg_read(UC_X86_REG_EBX)
-		r_ecx = mu.reg_read(UC_X86_REG_ECX)
-		r_edx = mu.reg_read(UC_X86_REG_EDX)
-		r_ebp = mu.reg_read(UC_X86_REG_EBP)
-		r_esp = mu.reg_read(UC_X86_REG_ESP)
-		r_edi = mu.reg_read(UC_X86_REG_EDI)
-		r_esi = mu.reg_read(UC_X86_REG_ESI)
-		r_eip = mu.reg_read(UC_X86_REG_EIP)
-		r_eflags = mu.reg_read(UC_X86_REG_EFLAGS)
-		r_CS = mu.reg_read(UC_X86_REG_CS)
-		r_SS = mu.reg_read(UC_X86_REG_SS)
-		r_DS = mu.reg_read(UC_X86_REG_DS)
-		r_ES = mu.reg_read(UC_X86_REG_ES)
-		print(">>> EAX = 0x%x" % r_eax)
-		print(">>> EBX = 0x%x" % r_ebx)
-		print(">>> ECX = 0x%x" % r_ecx)
-		print(">>> EDX = 0x%x" % r_edx)
-		print(">>> EBP = 0x%x" % r_ebp)
-		print(">>> ESP = 0x%x" % r_esp)
-		print(">>> EDI = 0x%x" % r_edi)
-		print(">>> ESI = 0x%x" % r_esi)
-		print(">>> EIP = 0x%x" % r_eip)
-		print(">>> CS = 0x%x" % r_CS)
-		print(">>> SS = 0x%x" % r_SS)
-		print(">>> DS = 0x%x" % r_DS)
-		print(">>> ES = 0x%x" % r_ES)
+		printReg(mu)
+		
 	
 	except Exception as e:
 		print(e)
